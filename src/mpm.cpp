3111f269f39abd3dbfdf59b08dc77addabc0fd1b
Lars Blatny <lkblatny@live.no>
2019-11-26 00:59:35 +0100

    third commit

--- src/mpm.cpp
+++ src/mpm.cpp
@@ -9,6 +9,7 @@
 //  * 3D
 //  * Cubic spline
 //  * adaptive timestepping
+//  * make Nx and Ny for grid
 
 typedef Eigen::Vector2d TV2;
 
@@ -27,8 +28,15 @@ inline double N(double x){
         return 0;
 		}
 }
-inline double wip(TV2 xp, TV2 xi, double h){
-    return N( (xp(0) - xi(0)) / h ) * N( (xp(1) - xi(1)) / h );
+inline double wip(TV2 xp, double xi, double yi, double h){
+    return N( (xp(0) - xi) / h ) * N( (xp(1) - yi) / h );
+}
+
+TV2 grad_wip(TV2 xp, double xi, double yi, double h){
+    TV2 out;
+    out(0) = 0;
+    out(1) = 0;
+    return out;
 }
 
 //  in : x, y column vectors, can be of type
@@ -55,22 +63,44 @@ class Particle{
 public:
 	TV2 x;
 	TV2 v;
-	double mass;
+  TV2 F;
 };
+
 
 class Simulation{
 public:
 	std::vector<Particle> particles;
+  unsigned int Np;
+  double particle_mass;
+  double particle_volume;
+  double rho;
+  double E;
+  double nu;
 	double dt;
 	double dx;
+
+  double mu;
+  double lambda;
+  void getElasticParams(){
+      mu = 0;
+      lambda = 0;
+  }
+
+  unsigned int N;
+  Eigen::MatrixXd grid_X;
+  Eigen::MatrixXd grid_Y;
+  Eigen::MatrixXd grid_VX;
+  Eigen::MatrixXd grid_VY;
+  Eigen::MatrixXd grid_mass;
+
+
 
 	void advanceStep();
 
 	// advanceStep uses the following:
 	void P2G();
+  void explicitEulerUpdate();
 	void G2P();
-	void explicitEuler();
-
 
 };
 
@@ -78,9 +108,36 @@ void Simulation::advanceStep(){
 
 }
 
+void Simulation::P2G(){
+
+}
+
+void Simulation::G2P(){
+
+}
+
+void Simulation::explicitEulerUpdate(){
+    TV2 force, velocity_increment;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            force = Eigen::Vector2d::Zero();
+            for(int p=0; p<Np; p++){
+                Eigen::JacobiSVD<Eigen::Matrix2d> svd((particles[p].F * particles[p].F.transpose()).array().log(), Eigen::ComputeThinU | Eigen::ComputeThinV);
+                TV2 sigma = svd.singularValues().array().exp().sqrt();
+                TV2 logSigma = sigma.array().log();
+                TV2 invSigma = sigma.inverse();
+                TV2 dPsidF = svd.matrixU() * ( 2*mu*invSigma*logSigma + lambda*logSigma.sum()*invSigma ) * svd.matrixV().transpose();
+                force -= particle_volume * dPsidF * particles[p].F.transpose() * grad_wip(particles[p].x, grid_X(i,j), grid_Y(i,j), dx);
+            }
+            velocity_increment = dt * force / grid_mass(i,j);
+            grid_VX(i,j) += velocity_increment(0);
+            grid_VY(i,j) += velocity_increment(1);
+        }
+    }
+}
+
 
 int main(){
     std::cout << "This is Larsie" << std::endl;
-
 		return 0;
 }
