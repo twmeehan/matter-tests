e1099b9a9d51a5954ca014d8779c7988c81cc733
Lars Blatny <lkblatny@live.no>
2019-11-26 00:03:23 +0100

    second commit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# SimpleMPM
+# LarsieMPM
 
-A simple implementation of the Material Point Method (MPM) in the elastoplastic framework.
+A simple implementation of the Material Point Method (MPM) in the elastoplastic framework. Currently only supports 2D and quadratic splines.
+
+`cd build`
+
+`cmake -DCMAKE_BUILD_TYPE=Release ..`
+
+`make`
--- src/mpm.cpp
+++ src/mpm.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cmath>
 #include <Eigen/Core>
 #include <Eigen/Dense>
@@ -11,8 +12,10 @@
 
 typedef Eigen::Vector2d TV2;
 
+///////////////////// TOOLS ////////////////////////
+
 // quadratic spline
-double N(double x){
+inline double N(double x){
     double xabs = std::abs(x);
     if (xabs < 0.5){
         return 0.75 - xabs * xabs;
@@ -24,10 +27,30 @@ double N(double x){
         return 0;
 		}
 }
-double wip(TV2 xp, TV2 xi, double h){
+inline double wip(TV2 xp, TV2 xi, double h){
     return N( (xp(0) - xi(0)) / h ) * N( (xp(1) - xi(1)) / h );
 }
 
+//  in : x, y column vectors, can be of type
+//       X, Y matrices, used to save the mesh
+void meshgrid(const Eigen::Matrix<double, -1, 1>& x,
+              const Eigen::Matrix<double, -1, 1>& y,
+              Eigen::Matrix<double, -1, -1>& X,
+              Eigen::Matrix<double, -1, -1>& Y) {
+  const long nx = x.size(), ny = y.size();
+  X.resize(ny, nx);
+  Y.resize(ny, nx);
+  for (long i = 0; i < ny; ++i) {
+    X.row(i) = x.transpose();
+  }
+  for (long j = 0; j < nx; ++j) {
+    Y.col(j) = y;
+  }
+}
+
+//////////////////////////////////////////////////////////////////
+
+// move to particle.hpp
 class Particle{
 public:
 	TV2 x;
@@ -48,7 +71,7 @@ public:
 	void G2P();
 	void explicitEuler();
 
-	
+
 };
 
 void Simulation::advanceStep(){
@@ -57,7 +80,7 @@ void Simulation::advanceStep(){
 
 
 int main(){
-
+    std::cout << "This is Larsie" << std::endl;
 
 		return 0;
 }
