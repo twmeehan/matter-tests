2d10e598a2253265ac3eb6c7cb5c3dd2e93ac125
Lars Blatny <lkblatny@live.no>
2019-11-26 16:00:18 +0100

    added remesh

--- src/mpm.cpp
+++ src/mpm.cpp
@@ -12,7 +12,7 @@
 //  * make Nx and Ny for grid
 
 typedef Eigen::Vector2d TV2;
-
+typedef Eigen::Matrix2d TM2;
 ///////////////////// TOOLS ////////////////////////
 
 // quadratic spline
@@ -32,7 +32,7 @@ inline double wip(TV2 xp, double xi, double yi, double h){
     return N( (xp(0) - xi) / h ) * N( (xp(1) - yi) / h );
 }
 
-TV2 grad_wip(TV2 xp, double xi, double yi, double h){
+TV2 grad_wip(double xp, double yp, double xi, double yi, double h){
     TV2 out;
     out(0) = 0;
     out(1) = 0;
@@ -58,55 +58,57 @@ void meshgrid(const Eigen::Matrix<double, -1, 1>& x,
 
 //////////////////////////////////////////////////////////////////
 
-// move to particle.hpp
 class Particle{
 public:
-	TV2 x;
-	TV2 v;
-  TV2 F;
+  TM2 F;
 };
-
 
 class Simulation{
 public:
-	std::vector<Particle> particles;
+  double dt;
+  double dx;
+
   unsigned int Np;
   double particle_mass;
   double particle_volume;
-  double rho;
-  double E;
-  double nu;
-	double dt;
-	double dx;
 
+	std::vector<Particle> particles;
+  Eigen::VectorXd particles_x;
+  Eigen::VectorXd particles_y;
+  Eigen::VectorXd particles_vx;
+  Eigen::VectorXd particles_vy;
+
+  double rho;
   double mu;
   double lambda;
-  void getElasticParams(){
-      mu = 0;
-      lambda = 0;
+
+  void setElasticParams(double E, double nu){
+      mu = nu * E / ( (1.0 + nu) * (1.0 - 2.0*nu) );
+      lambda = E / (2.0*(1.0+nu));
   }
 
-  unsigned int N;
+  void advanceStep(){
+      remesh();
+      P2G();
+      explicitEulerUpdate();
+      G2P();
+  };
+
+private:
+
+  unsigned int Nx, Ny;
   Eigen::MatrixXd grid_X;
   Eigen::MatrixXd grid_Y;
   Eigen::MatrixXd grid_VX;
   Eigen::MatrixXd grid_VY;
   Eigen::MatrixXd grid_mass;
 
-
-
-	void advanceStep();
-
-	// advanceStep uses the following:
+  void remesh();
 	void P2G();
   void explicitEulerUpdate();
 	void G2P();
 
-};
-
-void Simulation::advanceStep(){
-
-}
+}; // END mpm class
 
 void Simulation::P2G(){
 
@@ -116,20 +118,55 @@ void Simulation::G2P(){
 
 }
 
+void Simulation::remesh(){
+    double min_x = particles_x.minCoeff();
+    double min_y = particles_y.minCoeff();
+    double max_x = particles_x.maxCoeff();
+    double max_y = particles_y.maxCoeff();
+
+    double Lx = max_x - min_x;
+    double Ly = max_y - min_y;
+
+    Nx = std::ceil(Lx / dx);
+    Ny = std::ceil(Ly / dx);
+
+    double mid_x = min_x + Lx / 2.0;
+    double mid_y = min_y + Ly / 2.0;
+
+    double low_x  = mid_x - Nx*dx/2.0;
+    double low_y  = mid_y - Ny*dx/2.0;
+    double high_x = mid_x + Nx*dx/2.0;
+    double high_y = mid_y + Ny*dx/2.0;
+
+    Nx++;
+    Ny++;
+
+    Eigen::VectorXd lin_x = Eigen::VectorXd::LinSpaced(Nx, low_x, high_x);
+    Eigen::VectorXd lin_y = Eigen::VectorXd::LinSpaced(Ny, low_y, high_y);
+
+    meshgrid(lin_x, lin_y, grid_X, grid_Y);
+}
+
 void Simulation::explicitEulerUpdate(){
-    TV2 force, velocity_increment;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            force = Eigen::Vector2d::Zero();
+    for(int i=0; i<Nx; i++){
+        for(int j=0; j<Ny; j++){
+            double xi = grid_X(i,j);
+            double yi = grid_Y(i,j);
+            TV2 force = TV2::Zero();
             for(int p=0; p<Np; p++){
-                Eigen::JacobiSVD<Eigen::Matrix2d> svd((particles[p].F * particles[p].F.transpose()).array().log(), Eigen::ComputeThinU | Eigen::ComputeThinV);
-                TV2 sigma = svd.singularValues().array().exp().sqrt();
-                TV2 logSigma = sigma.array().log();
-                TV2 invSigma = sigma.inverse();
-                TV2 dPsidF = svd.matrixU() * ( 2*mu*invSigma*logSigma + lambda*logSigma.sum()*invSigma ) * svd.matrixV().transpose();
-                force -= particle_volume * dPsidF * particles[p].F.transpose() * grad_wip(particles[p].x, grid_X(i,j), grid_Y(i,j), dx);
+                double xp = particles_x(p);
+                double yp = particles_y(p);
+                if ( std::abs(xp-xi) < 1.5*dx || std::abs(xp-yi) < 1.5*dx){
+                    TM2 Fe = particles[p].F;
+                    Eigen::JacobiSVD<TM2> svd((Fe*Fe.transpose()).array().log(), Eigen::ComputeThinU | Eigen::ComputeThinV);
+                    TV2 sigma = svd.singularValues().array().exp().sqrt();
+                    TM2 logSigma = sigma.array().log().matrix().asDiagonal();
+                    TM2 invSigma = sigma.asDiagonal().inverse();
+                    TM2 dPsidF = svd.matrixU() * ( 2*mu*invSigma*logSigma + lambda*logSigma.trace()*invSigma ) * svd.matrixV().transpose();
+                    force -= particle_volume * dPsidF * Fe.transpose() * grad_wip(xp, yp, xi, yi, dx);
+                }
             }
-            velocity_increment = dt * force / grid_mass(i,j);
+            TV2 velocity_increment = dt * force / grid_mass(i,j);
             grid_VX(i,j) += velocity_increment(0);
             grid_VY(i,j) += velocity_increment(1);
         }
