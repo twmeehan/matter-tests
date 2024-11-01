25cc3d5afb6cc8d0c5d51a7711260b207c4c3144
Lars Blatny <lkblatny@live.no>
2024-11-01 13:47:52 +0100

    added rotation test

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -68,9 +68,72 @@ TEST(ElasticityTest, BulkModulus) {
 
 
 
+TEST(EnergyTest, Rotation) {
+
+    Simulation sim;
+    sim.save_sim = false;
+    sim.end_frame = 200;
+    sim.fps = 10;
+    sim.gravity = TV::Zero();
+    sim.cfl = 0.5;
+    sim.flip_ratio = -1;
+    sim.n_threads = 8;
+
+    sim.initialize(/* E */ 1e6, /* nu */ 0.3, /* rho */ 1550);
+
+    T h_gate, l_gate;
+    sim.Lx = 1;
+    sim.Ly = 1;
+    #ifdef THREEDIM
+        sim.Lz = 0.05;
+        SampleParticles(sim, sim.Lx, sim.Ly, sim.Lz, 0.01);
+    #else
+        SampleParticles(sim, sim.Lx, sim.Ly, 0.01);
+    #endif
+
+    T total_energy_init = 0;
+    for(int p = 0; p < sim.Np; p++){
+        sim.particles.x[p](0) -= 0.5*sim.Lx;
+        sim.particles.x[p](1) -= 0.5*sim.Ly;
+
+        T vx = -1.0*sim.particles.x[p](1) + 0.5;
+        T vy =  1.0*sim.particles.x[p](0) + 0.5;
+        sim.particles.v[p](0) = vx;
+        sim.particles.v[p](1) = vy;
+
+        total_energy_init += 0.5*(vx*vx + vy*vy); // per unit mass
+    }
+
+    sim.dt_max = 0.5 * sim.dx / sim.wave_speed;
+
+    sim.elastic_model = StvkWithHencky;
+    sim.plastic_model = NoPlasticity;
+
+    sim.simulate();
+
+    T total_energy_last = 0;
+    for(int p = 0; p < sim.Np; p++){
+        T vx = sim.particles.v[p](0);
+        T vy = sim.particles.v[p](1);
+        #ifdef THREEDIM
+            T vz = sim.particles.v[p](2);
+            total_energy_last += 0.5*(vx*vx + vy*vy + vz*vz); // per unit mass
+        #else
+            total_energy_last += 0.5*(vx*vx + vy*vy); // per unit mass
+        #endif
+    }
+
+    T rel_diff = (total_energy_init - total_energy_last) / total_energy_init;
+
+    EXPECT_TRUE((rel_diff >= 0) && (rel_diff <= 1e-3));
+    // Can not have energy increase!
+    // Energy decrease within a relative tolerance
+}
 
 
-TEST(GranularcollapseTest, DruckerPrager) {
+
+
+TEST(CollapseTest, DruckerPrager) {
 
     Simulation sim;
     sim.save_sim = false;
@@ -142,7 +205,5 @@ TEST(GranularcollapseTest, DruckerPrager) {
     auto max_x_it = std::max_element( sim.particles.x.begin(), sim.particles.x.end(), [](const TV &x1, const TV &x2){return x1(0) < x2(0);} );
     T max_x = (*max_x_it)(0);
     T diff = std::abs(max_x - 0.56);
-    // 2D: 0.550019
-    // 3D: 0.570681
     ASSERT_NEAR(diff, 0.0, 0.011);
 }
